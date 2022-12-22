afd7fff035e2290a3b718f06b63d455a68b277de
JacopoMarini <marinijacopo96@gmail.com>
2022-12-22 16:10:08 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import { Text } from './components/Text'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Cart } from './pages/Cart'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import { ProductDetail } from './pages/ProductDetail'
+import { Products } from './pages/Products'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/products',
+    element: <Products />
+  },
+  {
+    path: 'products/:id',
+    element: <ProductDetail />
+  },
+  {
+    path: 'cart',
+    element: <Cart />
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+])
 
 function App() {
-  return (
-    <div className="App">
-      <Text fontColor="green">Prova</Text>
-      <Text fontColor="red">Prova1</Text>
-      <Text fontColor="blue">Prova2</Text>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
