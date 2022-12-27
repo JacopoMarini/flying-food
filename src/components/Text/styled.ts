4e8ebf30078b46b1ab1700ecce7b54d911ab2872
JacopoMarini <marinijacopo96@gmail.com>
2022-12-27 10:36:47 +0100

    Refactorying text

--- src/components/Text/styled.ts
+++ src/components/Text/styled.ts
@@ -3,10 +3,6 @@ import { ThemeColor, FontSize } from '../../style/theme'
 
 export type TextVariant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span'
 
-const sizeMap: Partial<Record<TextVariant, FontSize>> = {
-  h1: 'xl',
-  h2: 'lg'
-}
 export const StyledText = styled.p<{
   textColor: ThemeColor
   variant: TextVariant
@@ -17,7 +13,7 @@ export const StyledText = styled.p<{
 }>`
   color: ${({ theme, textColor }) => theme.colors[textColor]};
   font-size: ${({ theme, textSize, variant }) => {
-    const size = textSize || sizeMap[variant] || 'md'
+    const size = textSize || (theme.textVariants[variant].size as FontSize) || 'md'
     return theme.fontSizes[size]
   }}rem;
   font-weight: ${({ bold }) => (bold ? 700 : 400)};
