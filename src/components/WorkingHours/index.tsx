24eb1fe6c5f0bb74b120d04da6e4b7e561ec11aa
JacopoMarini <marinijacopo96@gmail.com>
2022-12-28 12:32:35 +0100

    Add working hours

--- src/components/WorkingHours/index.tsx
+++ src/components/WorkingHours/index.tsx
@@ -1,6 +1,6 @@
 import { Icon } from '../Icon'
+import { Stack } from '../Stack'
 import { Text } from '../Text'
-import { StyledWorkingHours } from './styled'
 
 const variantColors = {
   light: {
@@ -23,14 +23,16 @@ export const WorkingHours = ({ variant, opening, closing }: Props) => {
   const { text, icon } = variantColors[variant]
 
   return (
-    <StyledWorkingHours>
-      <Icon name="clock" color={icon} size="lg" />
-      <Text variant="p" color={text} uppercase size="sm">
-        working hours
-      </Text>
-      <Text variant="p">
-        {opening} {closing}
-      </Text>
-    </StyledWorkingHours>
+    <Stack direction="horizontal" gap={16}>
+      <Icon name="clock" color={icon} size="2x" />
+      <div>
+        <Text variant="p" color={text} uppercase size="sm">
+          working hours
+        </Text>
+        <Text variant="p" color={text} size="sm">
+          {opening} {closing}
+        </Text>
+      </div>
+    </Stack>
   )
 }
--- src/components/WorkingHours/styled.ts
+++ src/components/WorkingHours/styled.ts
@@ -1,3 +1,7 @@
 import styled from '@emotion/styled'
 
-export const StyledWorkingHours = styled.div``
+export const StyledWorkingHours = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`
